eea720dee1c2be151744a7292f488d93e195f8d4
Karter13 <KarMiH1975@mail.ru>
2020-12-05 14:34:31 +0300

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import set = Reflect.set;
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'Dynamic title';
   number = 40;
   arr = [1, 2, 3];
   obj = {name: 'Maikl', age: '45'};
+
+  img = 'https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.img = 'https://miro.medium.com/max/588/1*15CYVZdpsxir8KLdxEZytg.png';
+    }, 5000);
+  }
 
 }
