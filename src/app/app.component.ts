8bf8336adb6480bda5b83d9c41d14e3ad23e4887
Karter13 <KarMiH1975@mail.ru>
2020-12-05 16:34:52 +0300

    [ngStyle] [ngClass] [class]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,32 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  backgroundToggle = false;
 
-  title = 'Dynamic title';
-  number = 40;
-  arr = [1, 2, 3];
-  obj = {name: 'Maikl', age: '45'};
-
-  // img = 'https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png';
-
-  inputValue = '';
-
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is over');
-    //   this.img = 'https://miro.medium.com/max/588/1*15CYVZdpsxir8KLdxEZytg.png';
-    // }, 5000);
-  }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (event.target as HTMLInputElement).value;
-  }
-
-  onClick() {
-    console.log('click!');
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
+  onToggle() {
+    this.backgroundToggle = !this.backgroundToggle;
   }
 }
