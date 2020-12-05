cc56eef71dc14cb442b2bc569dff6285fb5eabfb
Karter13 <KarMiH1975@mail.ru>
2020-12-05 14:14:25 +0300

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'Dynamic title';
+  number = 40;
+  arr = [1, 2, 3];
+  obj = {name: 'Maikl', age: '45'};
+
 }
