35e95f48475c179c3541716d578da67aa0c20282
Karter13 <KarMiH1975@mail.ru>
2020-12-05 15:48:56 +0300

    Event bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import set = Reflect.set;
 
 @Component({
   selector: 'app-root',
@@ -13,13 +12,26 @@ export class AppComponent {
   arr = [1, 2, 3];
   obj = {name: 'Maikl', age: '45'};
 
-  img = 'https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png';
+  // img = 'https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png';
+
+  inputValue = '';
 
   constructor() {
-    setTimeout(() => {
-      console.log('Timeout is over');
-      this.img = 'https://miro.medium.com/max/588/1*15CYVZdpsxir8KLdxEZytg.png';
-    }, 5000);
+    // setTimeout(() => {
+    //   console.log('Timeout is over');
+    //   this.img = 'https://miro.medium.com/max/588/1*15CYVZdpsxir8KLdxEZytg.png';
+    // }, 5000);
   }
 
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
+
+  onClick() {
+    console.log('click!');
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
 }
