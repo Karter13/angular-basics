76a8caff4715c88f2fd4687966b20654c83a31d3
Karter13 <KarMiH1975@mail.ru>
2020-12-06 21:41:43 +0300

    native pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 
+export interface Post {
+  title: string,
+  text: string,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
+  search = '';
+  searchField = 'title';
+
+  posts: Array<Post> = [
+    {title: 'Angular', text: 'I started study Angular'},
+    {title: 'React', text: 'I like React'},
+    {title: 'Lodash', text: 'I do not now Lodash'},
+  ];
+
+  addNewPost() {
+    this.posts.unshift({
+      title: 'Maikl',
+      text: 'He likes React and Angular '
+    });
   }
-
-
 }
