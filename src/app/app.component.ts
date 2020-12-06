f70572d3837f10d6c6b2c81c8d1439dd946daa6b
Karter13 <KarMiH1975@mail.ru>
2020-12-06 13:33:31 +0300

    lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 export interface Post {
   title: string;
@@ -11,14 +11,30 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   posts: Post[] = [
     {title: 'Первый блок!', text: 'Я хочу выучить Angular компоненты', id: 1},
-    {title: 'Следующий блое!', text: 'Я буду разрабатывать на Angular', id: 1},
+    // {title: 'Следующий блое!', text: 'Я буду разрабатывать на Angular', id: 2},
   ];
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('TimeOut');
+      this.posts[0] = {
+        title: 'Change!',
+        text: 'WWWWWWWWWWWWWWWWWW',
+        id: 1
+      };
+    }, 5000);
+  }
 
   updatePosts(post: Post) {
     this.posts.unshift(post);
+  }
+
+  removePost(id: number) {
+    // console.log('remove id', id);
+    this.posts = this.posts.filter(post => post.id !== id);
   }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,18 +1,46 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewEncapsulation
+} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnDestroy {
 
-  @Input() post: Post | undefined;
+  @Input() post!: Post;
+  @Output() onRemove = new EventEmitter<number>();
+  @ContentChild('info', {static: true}) infoRef!: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+  }
+
+  removePost() {
+    this.onRemove.emit(this.post.id);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+    // console.log(this.infoRef.nativeElement);
   }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
 
 }
