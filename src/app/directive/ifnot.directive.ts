8c49f6e6e9bc5743aae6225c2227bea13178b1e7
Karter13 <KarMiH1975@mail.ru>
2020-12-06 18:30:12 +0300

    created IfnotDirective

--- src/app/directive/ifnot.directive.ts
+++ src/app/directive/ifnot.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[appIfnot]'
 })
 export class IfnotDirective {
 
-  constructor() { }
+  @Input('appIfnot') set ifNot(condition: boolean) {
+    if(!condition) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>,
+              private viewContainer: ViewContainerRef ) { }
 
 }
