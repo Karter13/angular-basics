dda49ba1fb4029b5d38fcd25fe2609c76581b5e2
Karter13 <KarMiH1975@mail.ru>
2020-12-07 23:11:46 +0300

    created GET HttpRequest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,8 +14,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  todos: Todo[] = [];
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get<Array<Todo>>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos => {
+        console.log('Response: ', todos);
+        this.todos = todos;
+      });
   }
 }
 
