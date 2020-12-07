d7e191977d41eabb5b1de977dd5ce0db773378a3
Karter13 <KarMiH1975@mail.ru>
2020-12-07 23:30:23 +0300

    created POST HttpRequest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   todos: Todo[] = [];
+  todoTitle = '';
 
   constructor(private http: HttpClient) {
   }
@@ -24,6 +25,21 @@ export class AppComponent implements OnInit {
       .subscribe(todos => {
         console.log('Response: ', todos);
         this.todos = todos;
+      });
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false,
+    };
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log('NewTodo: ', todo);
+        this.todos.unshift(todo);
       });
   }
 }
