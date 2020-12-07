cb83dda853613c2e738f6400cd8e0d6b260e88e0
Karter13 <KarMiH1975@mail.ru>
2020-12-07 14:30:38 +0300

    created HTML and styles for form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
-
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() => {
-      resolve('Prpomise Resoloved');
-    }, 4000);
-  });
-
-  date$: Observable<Date> = new Observable( obs => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
-
-  date?: Date;
-
-  ngOnInit(): void {
-    this.date$.subscribe(date => {
-      this.date = date;
-    });
-  }
+export class AppComponent {
 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
-import {MultByPipe} from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe,
-    ExMarksPipe,
-    FilterPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
