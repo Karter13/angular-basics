975e18e0546af1fd46fc6a3c22715fa3df8ae8af
Karter13 <KarMiH1975@mail.ru>
2020-12-07 17:04:12 +0300

    created Validators for form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form!: FormGroup;
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+    });
+  }
+
+  submit() {
+    if(this.form.valid) {
+      console.log('Form: ', this.form);
+      const formData = {...this.form.value};
+      console.log('FormData:', formData);
+    }
+  }
 }
 
