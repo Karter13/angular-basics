9e0b47bd41e02a796b6b6ccc23fa0816be985da5
Karter13 <KarMiH1975@mail.ru>
2020-12-07 20:03:36 +0300

    added selected city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,15 +20,36 @@ export class AppComponent implements OnInit {
         Validators.required,
         Validators.minLength(9)
       ]),
+      address: new FormGroup({
+        country: new FormControl('by'),
+        city: new FormControl('Гродно', Validators.required)
+      })
+
     });
   }
 
   submit() {
-    if(this.form.valid) {
+    if (this.form.valid) {
       console.log('Form: ', this.form);
       const formData = {...this.form.value};
       console.log('FormData:', formData);
     }
+  }
+
+  setCapital() {
+    const cityMap  = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск',
+    };
+
+    // @ts-ignore
+    const cityKey = this.form.get('address').get('country').value;
+    // @ts-ignore
+    const city = cityMap[cityKey];
+
+    this.form.patchValue({address: {city}});
+
   }
 }
 
