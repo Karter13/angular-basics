05f40ca5cd08100e77cf69c6314782c65abc70f4
Karter13 <KarMiH1975@mail.ru>
2020-12-10 23:26:33 +0300

    step animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, keyframes, query, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -20,24 +20,42 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
       transition('special <=> *', [
-        style({background: 'green'}),
-        animate('2s', style({
-          background: 'pink'
-        })),
-        animate(750)
+        group([
+          query('h4', animate(1500, style({
+            fontSize: '.5rem',
+            color: 'red',
+          }))),
+          style({background: 'green'}),
+          animate('2s', style({
+            background: 'pink'
+          })),
+          animate(750)
+        ])
       ]),
       // void => *
       transition(':enter', [
-        style({opacity: 0}),
-        animate('1000ms ease-out')
+        animate('4s', keyframes([
+          style({background: 'red', offset: 0}),
+          style({background: 'black', offset: 0.2}),
+          style({background: 'orange', offset: 0.3}),
+          style({background: 'blue', offset: 1}),
+        ]))
+        // style({opacity: 0}),
+        // animate('1000ms ease-out')
       ]),
       // * => void
       transition(':leave', [
         style({opacity: 1}),
-        animate(1000, style({
-          opacity: 0,
-          transform: 'scale(1.2)'
-        }))
+        group([
+          animate(1000, style({
+            opacity: 0,
+            transform: 'scale(1.2)'
+          })),
+          animate(400, style({
+            color: '#000',
+            fontWeight: 'bold',
+          }))
+        ])
       ])
     ])
   ]
@@ -48,5 +66,13 @@ export class AppComponent {
 
   animate() {
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
+  }
+
+  animationStarted(event: AnimationEvent) {
+    console.log('animationStarted', event);
+  }
+
+  animationDone(event: AnimationEvent) {
+    console.log('animationDone', event);
   }
 }
