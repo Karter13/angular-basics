8c6169b091498fecae2f34ac3d648f388441e252
Karter13 <KarMiH1975@mail.ru>
2020-12-19 20:08:25 +0300

    unsubscribe Rxjs

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,40 +1,52 @@
 import {fromEvent, Observable} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, map, takeUntil} from 'rxjs/operators';
 
 
-const search$ = new Observable<Event>(observer => {
-  const search = document.getElementById('search');
-  const stop = document.getElementById('stop');
-  if (!search || !stop) {
-    observer.error('Element does not exists on the page');
-    return;
-  }
-
-  const onSearch = (event: Event | undefined) => {
-    console.log(123);
-    observer.next(event);
-  };
-
-  const onStop = () => {
-    observer.complete();
-    clear();
-  };
-
-  // @ts-ignore
-  search.addEventListener('input', onSearch);
-  stop.addEventListener('click', onStop);
-
-  const clear = () => {
-    search.removeEventListener('input', onSearch);
-    stop.removeEventListener('click', onStop);
-  };
-});
-
-
-// const search$: Observable<Event> = fromEvent<Event>(
+// const search$ = new Observable<Event>(observer => {
+//   const search = document.getElementById('search');
+//   const stop = document.getElementById('stop');
+//   if (!search || !stop) {
+//     observer.error('Element does not exists on the page');
+//     return;
+//   }
+//
+//   const onSearch = (event: Event | undefined) => {
+//     checkSubscription();
+//     observer.next(event);
+//   };
+//
+//   const onStop = () => {
+//     checkSubscription();
+//     observer.complete();
+//     clear();
+//   };
+//
 //   // @ts-ignore
-//   document.getElementById('search'), 'input'
-// );
+//   search.addEventListener('input', onSearch);
+//   stop.addEventListener('click', onStop);
+//
+//   const checkSubscription = () => {
+//     if (observer.closed) {
+//       clear();
+//     }
+//   };
+//
+//   const clear = () => {
+//     search.removeEventListener('input', onSearch);
+//     stop.removeEventListener('click', onStop);
+//   };
+// });
+
+
+const search$: Observable<Event> = fromEvent<Event>(
+  // @ts-ignore
+  document.getElementById('search'), 'input'
+);
+
+const stop$: Observable<Event> = fromEvent<Event>(
+  // @ts-ignore
+  document.getElementById('stop'), 'click'
+);
 
 const searchSubscription = search$.pipe(
   map(event => {
@@ -42,14 +54,20 @@ const searchSubscription = search$.pipe(
   }),
   debounceTime(1000),
   map(value => value.length > 3 ? value : ''),
-  distinctUntilChanged()
+  distinctUntilChanged(),
+  takeUntil(stop$)
 ).subscribe(value => {
   console.log(value);
 });
 
-setTimeout(() => {
-  console.log('unsub');
-  searchSubscription.unsubscribe();
-}, 10000);
+// setTimeout(() => {
+//   console.log('unsub');
+//   searchSubscription.unsubscribe();
+// }, 10000);
 
+// unsubscribe === takeUntil(stop$)
+// const stopSubscription = stop$.subscribe(() => {
+//   searchSubscription.unsubscribe();
+//   stopSubscription.unsubscribe();
+// });
 
